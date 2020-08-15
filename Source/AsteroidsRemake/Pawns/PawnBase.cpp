9821f7214aff0295aab8657ab510776375ce6038
StevenEsposito7 <stevenesposito7@gmail.com>
2020-08-15 01:38:08 -0400

    Added PawnBase

---
+++ Source/AsteroidsRemake/Pawns/PawnBase.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnBase.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APawnBase::APawnBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	HitBoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	RootComponent = HitBoxComp;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(RootComponent);
+
+	SpawnPointComp = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	SpawnPointComp->SetupAttachment(MeshComp);
+}
+
+void APawnBase::PawnDefeated()
+{
+	DestroyPawn();
+}
+
+void APawnBase::DestroyPawn()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, DeathParticle, GetActorLocation());
+	UGameplayStatics::PlaySoundAtLocation(this, DeathSFX, GetActorLocation());
+}
---
+++ Source/AsteroidsRemake/Pawns/PawnBase.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PawnBase.generated.h"
+
+class UBoxComponent;
+//class UHealthComponent;
+
+UCLASS()
+class ASTEROIDSREMAKE_API APawnBase : public APawn
+{
+	GENERATED_BODY()
+
+private:
+
+	/** Components */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UBoxComponent* HitBoxComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* MeshComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* SpawnPointComp;
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		//UHealthComponent* HealthComp;
+
+	/** Effects */
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		UParticleSystem* DeathParticle;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		USoundBase* DeathSFX;		
+
+public:
+	// Sets default values for this pawn's properties
+	APawnBase();
+
+	void PawnDefeated();
+	virtual void DestroyPawn();
+
+};
