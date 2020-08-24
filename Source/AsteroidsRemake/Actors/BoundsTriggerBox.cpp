3556ccb9c4ee673225d5f7a59b20b9248aa4cb4a
StevenEsposito7 <stevenesposito7@gmail.com>
2020-08-23 23:59:35 -0400

    Tweaked Bound Trigger Box Code

--- Source/AsteroidsRemake/Actors/BoundsTriggerBox.cpp
+++ Source/AsteroidsRemake/Actors/BoundsTriggerBox.cpp
@@ -29,13 +29,13 @@ void ABoundsTriggerBox::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherAct
 		if (XOffset != 0)
 		{
 			OtherActor->SetActorLocation(FVector(TargetTriggerBox->GetActorLocation().X + 
-				(TargetTriggerBox->GetComponentsBoundingBox().GetExtent().X + OtherActor->GetComponentsBoundingBox().GetExtent().X) * XOffset,
+				(TargetTriggerBox->GetComponentsBoundingBox().GetExtent().X + OtherActor->GetComponentsBoundingBox().GetSize().X) * XOffset,
 				OtherActor->GetActorLocation().Y, OtherActor->GetActorLocation().Z));
 		}
 		else if (YOffset != 0)
 		{
 			OtherActor->SetActorLocation(FVector(OtherActor->GetActorLocation().X, TargetTriggerBox->GetActorLocation().Y +
-				(TargetTriggerBox->GetComponentsBoundingBox().GetExtent().Y + OtherActor->GetComponentsBoundingBox().GetExtent().Y) * YOffset,
+				(TargetTriggerBox->GetComponentsBoundingBox().GetExtent().Y + OtherActor->GetComponentsBoundingBox().GetSize().Y) * YOffset,
 				OtherActor->GetActorLocation().Z));
 		}
 	}
